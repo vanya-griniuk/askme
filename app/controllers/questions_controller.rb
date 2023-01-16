fce7f9f6587e43553100c1d87df6af8cdf64f56f
vanya-griniuk <vanyagriniuk@gmail.com>
2023-01-16 11:37:38 +0200

    add routes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,24 @@
 class QuestionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
+    Question.create(
+      body: params[:question][:body],
+      user_id: params[:question][:user_id]
+    )
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question = Question.update(
+      body: params[:question][:body],
+      user_id: params[:question][:user_id]
+    )
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    
   end
 end
