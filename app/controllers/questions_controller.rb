1e01993697b3a1cd05914822135ea03cf8c21ecb
vanya-griniuk <vanyagriniuk@gmail.com>
2023-01-17 13:54:09 +0200

    code refactoring

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,32 +1,24 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: %i[unpdate show destroy edit]
   def create
-    question = Question.create(
-      body: params[:question][:body],
-      user_id: params[:question][:user_id]
-    )
+    question = Question.create(question_params)
 
     redirect_to question_path(question)
   end
 
   def update
-    @question = Question.find(params[:id])
-    @question = Question.update(
-      body: params[:question][:body],
-      user_id: params[:question][:user_id]
-    )
+    @question = Question.update(question_params)
     
     redirect_to question_path(@question)
   end
 
   def destroy
-    @question = Question.find(params[:id])
     @question.destroy
 
     redirect_to questions_path
   end
 
   def show
-    @question = Question.find(params[:id])
   end
 
   def index
@@ -38,6 +30,16 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body, :user_id)
+  end
+
+  def set_question
     @question = Question.find(params[:id])
   end
+
 end
